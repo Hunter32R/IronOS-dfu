6bd23cbab61ce4b5c1fc47ec7918b6a378a57544
Hunter32R <127041035+Hunter32R@users.noreply.github.com>
2025-04-20 10:18:19 +0300

    Update oled.h

--- src/oled.h
+++ src/oled.h
@@ -6,28 +6,18 @@
 
 #define DEVICEADDR_OLED (0x3c << 1)
 
-#if MODEL_S60 || MODEL_S60P || MODEL_S99
-#define OLED_AREA_X      0
-#define OLED_AREA_Y      0
-#define OLED_WIDTH       16
-#define OLED_HEIGHT      96
-#define OLED_GRAM_START  0x00
-#define OLED_GRAM_END    0x7F
-#define OLED_VCOM_LAYOUT 0x12
-
-#ifdef OLED_FLIP_BUFFER
-#define OLED_SEGMENT_MAP 0xA0
-#else
-#define OLED_SEGMENT_MAP 0xA1
-#endif
-
-#if MODEL_TS101
+#if MODEL_S60 || MODEL_S60P || MODEL_S99 || MODEL_TS101
 #define OLED_AREA_X      0
 #define OLED_AREA_Y      0
 #define OLED_WIDTH       96
 #define OLED_HEIGHT      16
 #define OLED_VCOM_LAYOUT 0x02
 
+#ifdef OLED_FLIP_BUFFER
+#define OLED_SEGMENT_MAP 0xA0
+#else
+#define OLED_SEGMENT_MAP 0xA1
+#endif
 #else
 
 #define OLED_AREA_X      0
